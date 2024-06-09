d7f52ecfcff88f3d396c33cf56387dcf3ec21cd4
Juan Serrano Soria <54719300+juan-serrano-soria@users.noreply.github.com>
2024-06-09 22:54:21 +0900

    Add basic player rectangles

--- main.lua
+++ main.lua
@@ -1,11 +1,23 @@
 function love.load()
-
+    y = 300 -- Player 1
+    yy = 300 -- Player 2
 end
 
 function love.update(dt)
-
+    -- Movement of the players
+    if love.keyboard.isDown("w") then
+        y = y - 5
+    elseif love.keyboard.isDown("s") then
+        y = y + 5
+    end
+    if love.keyboard.isDown("up") then
+        yy = yy - 5
+    elseif love.keyboard.isDown("down") then
+        yy = yy + 5
+    end
 end
 
 function love.draw()
-
+    love.graphics.rectangle("fill", 10, y - 100/2, 5, 100) -- Player 1
+    love.graphics.rectangle("fill", 790 - 5, yy - 100/2, 5, 100) -- Player 2
 end
